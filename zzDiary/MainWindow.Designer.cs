c663d4327938da55a49a5cffb50a2d7436142ff3
Wenfei Cao <cwfroom123@gmail.com>
2019-08-26 16:57:51 +0900

    Move diary UI elements to diary tab

--- zzDiary/MainWindow.Designer.cs
+++ zzDiary/MainWindow.Designer.cs
@@ -48,42 +48,47 @@ namespace zzDiary
             this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.YearList = new System.Windows.Forms.ListBox();
             this.MonthList = new System.Windows.Forms.ListBox();
-            this.sortButton = new System.Windows.Forms.Button();
+            this.SortButton = new System.Windows.Forms.Button();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.DiaryPage = new System.Windows.Forms.TabPage();
+            this.RecordPage = new System.Windows.Forms.TabPage();
             this.StatusStrip.SuspendLayout();
+            this.TabControl.SuspendLayout();
+            this.DiaryPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // EntryList
             // 
             this.EntryList.FormattingEnabled = true;
             this.EntryList.ItemHeight = 21;
-            this.EntryList.Location = new System.Drawing.Point(11, 10);
+            this.EntryList.Location = new System.Drawing.Point(6, 1);
             this.EntryList.Name = "EntryList";
-            this.EntryList.Size = new System.Drawing.Size(269, 529);
+            this.EntryList.Size = new System.Drawing.Size(269, 508);
             this.EntryList.TabIndex = 1;
             this.EntryList.SelectedIndexChanged += new System.EventHandler(this.EntryList_SelectedIndexChanged);
             // 
             // TitleBox
             // 
-            this.TitleBox.Location = new System.Drawing.Point(371, 11);
+            this.TitleBox.Location = new System.Drawing.Point(368, 2);
             this.TitleBox.Name = "TitleBox";
-            this.TitleBox.Size = new System.Drawing.Size(594, 28);
+            this.TitleBox.Size = new System.Drawing.Size(607, 28);
             this.TitleBox.TabIndex = 3;
             this.TitleBox.TextChanged += new System.EventHandler(this.TitleBox_TextChanged);
             // 
             // ContentBox
             // 
             this.ContentBox.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.ContentBox.Location = new System.Drawing.Point(285, 44);
+            this.ContentBox.Location = new System.Drawing.Point(281, 37);
             this.ContentBox.Multiline = true;
             this.ContentBox.Name = "ContentBox";
             this.ContentBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ContentBox.Size = new System.Drawing.Size(679, 495);
+            this.ContentBox.Size = new System.Drawing.Size(694, 470);
             this.ContentBox.TabIndex = 4;
             this.ContentBox.TextChanged += new System.EventHandler(this.ContentBox_TextChanged);
             // 
             // YearBox
             // 
-            this.YearBox.Location = new System.Drawing.Point(286, 11);
+            this.YearBox.Location = new System.Drawing.Point(281, 2);
             this.YearBox.MaxLength = 2;
             this.YearBox.Name = "YearBox";
             this.YearBox.ReadOnly = true;
@@ -93,7 +98,7 @@ namespace zzDiary
             // 
             // MonthBox
             // 
-            this.MonthBox.Location = new System.Drawing.Point(313, 11);
+            this.MonthBox.Location = new System.Drawing.Point(308, 2);
             this.MonthBox.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
             this.MonthBox.MaxLength = 2;
             this.MonthBox.Name = "MonthBox";
@@ -104,7 +109,7 @@ namespace zzDiary
             // 
             // DayBox
             // 
-            this.DayBox.Location = new System.Drawing.Point(339, 11);
+            this.DayBox.Location = new System.Drawing.Point(335, 2);
             this.DayBox.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
             this.DayBox.MaxLength = 2;
             this.DayBox.Name = "DayBox";
@@ -115,7 +120,7 @@ namespace zzDiary
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(1015, 505);
+            this.SaveButton.Location = new System.Drawing.Point(1016, 468);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(83, 34);
             this.SaveButton.TabIndex = 9;
@@ -125,7 +130,7 @@ namespace zzDiary
             // 
             // NewButton
             // 
-            this.NewButton.Location = new System.Drawing.Point(1015, 311);
+            this.NewButton.Location = new System.Drawing.Point(1016, 268);
             this.NewButton.Name = "NewButton";
             this.NewButton.Size = new System.Drawing.Size(83, 34);
             this.NewButton.TabIndex = 5;
@@ -135,7 +140,7 @@ namespace zzDiary
             // 
             // UpButton
             // 
-            this.UpButton.Location = new System.Drawing.Point(1015, 349);
+            this.UpButton.Location = new System.Drawing.Point(1016, 308);
             this.UpButton.Name = "UpButton";
             this.UpButton.Size = new System.Drawing.Size(83, 34);
             this.UpButton.TabIndex = 6;
@@ -145,7 +150,7 @@ namespace zzDiary
             // 
             // DownButton
             // 
-            this.DownButton.Location = new System.Drawing.Point(1015, 388);
+            this.DownButton.Location = new System.Drawing.Point(1016, 348);
             this.DownButton.Name = "DownButton";
             this.DownButton.Size = new System.Drawing.Size(83, 34);
             this.DownButton.TabIndex = 7;
@@ -155,7 +160,7 @@ namespace zzDiary
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(1015, 427);
+            this.DeleteButton.Location = new System.Drawing.Point(1016, 388);
             this.DeleteButton.Name = "DeleteButton";
             this.DeleteButton.Size = new System.Drawing.Size(83, 34);
             this.DeleteButton.TabIndex = 8;
@@ -178,6 +183,7 @@ namespace zzDiary
             // 
             // WordCountLabel
             // 
+            this.WordCountLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.WordCountLabel.Name = "WordCountLabel";
             this.WordCountLabel.Size = new System.Drawing.Size(99, 21);
             this.WordCountLabel.Text = "WordCount";
@@ -185,6 +191,7 @@ namespace zzDiary
             // 
             // StatusLabel
             // 
+            this.StatusLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StatusLabel.Name = "StatusLabel";
             this.StatusLabel.Size = new System.Drawing.Size(57, 21);
             this.StatusLabel.Text = "Status";
@@ -193,7 +200,7 @@ namespace zzDiary
             // 
             this.YearList.FormattingEnabled = true;
             this.YearList.ItemHeight = 21;
-            this.YearList.Location = new System.Drawing.Point(980, 11);
+            this.YearList.Location = new System.Drawing.Point(981, 2);
             this.YearList.Name = "YearList";
             this.YearList.Size = new System.Drawing.Size(73, 256);
             this.YearList.TabIndex = 11;
@@ -202,46 +209,77 @@ namespace zzDiary
             // 
             this.MonthList.FormattingEnabled = true;
             this.MonthList.ItemHeight = 21;
-            this.MonthList.Location = new System.Drawing.Point(1063, 11);
+            this.MonthList.Location = new System.Drawing.Point(1060, 2);
             this.MonthList.Name = "MonthList";
             this.MonthList.Size = new System.Drawing.Size(73, 256);
             this.MonthList.TabIndex = 12;
             // 
-            // sortButton
+            // SortButton
             // 
-            this.sortButton.Location = new System.Drawing.Point(1015, 466);
-            this.sortButton.Name = "sortButton";
-            this.sortButton.Size = new System.Drawing.Size(83, 34);
-            this.sortButton.TabIndex = 14;
-            this.sortButton.Text = "Sort";
-            this.sortButton.UseVisualStyleBackColor = true;
-            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            this.SortButton.Location = new System.Drawing.Point(1016, 428);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(83, 34);
+            this.SortButton.TabIndex = 14;
+            this.SortButton.Text = "Sort";
+            this.SortButton.UseVisualStyleBackColor = true;
+            this.SortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.DiaryPage);
+            this.TabControl.Controls.Add(this.RecordPage);
+            this.TabControl.Location = new System.Drawing.Point(0, -1);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(1149, 544);
+            this.TabControl.TabIndex = 15;
+            // 
+            // DiaryPage
+            // 
+            this.DiaryPage.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.DiaryPage.Controls.Add(this.EntryList);
+            this.DiaryPage.Controls.Add(this.YearBox);
+            this.DiaryPage.Controls.Add(this.MonthList);
+            this.DiaryPage.Controls.Add(this.MonthBox);
+            this.DiaryPage.Controls.Add(this.YearList);
+            this.DiaryPage.Controls.Add(this.DayBox);
+            this.DiaryPage.Controls.Add(this.TitleBox);
+            this.DiaryPage.Controls.Add(this.ContentBox);
+            this.DiaryPage.Controls.Add(this.DeleteButton);
+            this.DiaryPage.Controls.Add(this.DownButton);
+            this.DiaryPage.Controls.Add(this.UpButton);
+            this.DiaryPage.Controls.Add(this.NewButton);
+            this.DiaryPage.Controls.Add(this.SortButton);
+            this.DiaryPage.Controls.Add(this.SaveButton);
+            this.DiaryPage.Location = new System.Drawing.Point(4, 30);
+            this.DiaryPage.Name = "DiaryPage";
+            this.DiaryPage.Padding = new System.Windows.Forms.Padding(3);
+            this.DiaryPage.Size = new System.Drawing.Size(1141, 510);
+            this.DiaryPage.TabIndex = 0;
+            this.DiaryPage.Text = "Diary";
+            // 
+            // RecordPage
+            // 
+            this.RecordPage.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.RecordPage.Location = new System.Drawing.Point(4, 30);
+            this.RecordPage.Name = "RecordPage";
+            this.RecordPage.Padding = new System.Windows.Forms.Padding(3);
+            this.RecordPage.Size = new System.Drawing.Size(1141, 510);
+            this.RecordPage.TabIndex = 1;
+            this.RecordPage.Text = "Record";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1149, 572);
-            this.Controls.Add(this.sortButton);
-            this.Controls.Add(this.MonthList);
-            this.Controls.Add(this.YearList);
+            this.Controls.Add(this.TabControl);
             this.Controls.Add(this.StatusStrip);
-            this.Controls.Add(this.DeleteButton);
-            this.Controls.Add(this.DownButton);
-            this.Controls.Add(this.UpButton);
-            this.Controls.Add(this.NewButton);
-            this.Controls.Add(this.SaveButton);
-            this.Controls.Add(this.DayBox);
-            this.Controls.Add(this.MonthBox);
-            this.Controls.Add(this.YearBox);
-            this.Controls.Add(this.ContentBox);
-            this.Controls.Add(this.TitleBox);
-            this.Controls.Add(this.EntryList);
             this.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1165, 611);
             this.MinimumSize = new System.Drawing.Size(1165, 611);
@@ -251,6 +289,9 @@ namespace zzDiary
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainWindowKeyPress);
             this.StatusStrip.ResumeLayout(false);
             this.StatusStrip.PerformLayout();
+            this.TabControl.ResumeLayout(false);
+            this.DiaryPage.ResumeLayout(false);
+            this.DiaryPage.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -273,8 +314,11 @@ namespace zzDiary
         private ToolStripStatusLabel StatusLabel;
         private ListBox YearList;
         private ListBox MonthList;
-        private Button sortButton;
+        private Button SortButton;
         private ToolStripStatusLabel WordCountLabel;
+        private TabControl TabControl;
+        private TabPage DiaryPage;
+        private TabPage RecordPage;
     }
 }
 
